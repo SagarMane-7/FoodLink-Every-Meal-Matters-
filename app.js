399eebde6e0ddf7eb1de657ad5f84773f25e0eb0
Sagar Mane <manesagar2120@gmail.com>
2025-03-17 22:00:44 +0530

    Fix images paths and logo for Render deployment

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(session({
   cookie: { secure: false }
 }));
 
-const port = process.env.PORT || 1000;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App is listening on ${port}`);
 });
